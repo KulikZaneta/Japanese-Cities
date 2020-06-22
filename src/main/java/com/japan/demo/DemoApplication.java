c592943c4b1e2593cd0d3f20000a499c69299323
Żaneta <kul.zaneta@gmail.com>
2020-06-22 11:22:48 +0200

    formatted code

--- src/main/java/com/japan/demo/DemoApplication.java
+++ src/main/java/com/japan/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.japan.demo;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -11,5 +12,4 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
-
 }
